396fa017b323681aea99aeb1e77390f8c83c8b43
Vladimir Shestakov <boolive@yandex.ru>
2014-11-17 09:32:58 +0500

    added class Transform

---
+++ utils/Transform.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Transform
+ * @aurhor Vladimir Shestakov
+ * @version 1.0
+ */
+namespace boolive\core\utils;
+
+class Transform
+{
+    static function transform($file, &$errors, $args)
+    {
+        return $file;
+    }
+}
